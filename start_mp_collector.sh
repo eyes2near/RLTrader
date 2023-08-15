f36525df24b355308225c1f370aa6975e7a82a66
eyes2near <eyes2near@gmail.com>
2023-08-15 16:21:58 +0800

    add cmd

---
+++ start_mp_collector.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+nohup python mp_collect_server.py --gpu 0 --num_worker 10 --num_env 3 --max_steps 50 --port 29550 > ./logs/29550.log &
+nohup python mp_collect_server.py --gpu 0 --num_worker 10 --num_env 3 --max_steps 50 --port 29551 > ./logs/29551.log &
+nohup python mp_collect_server.py --gpu 0 --num_worker 10 --num_env 3 --max_steps 50 --port 29552 > ./logs/29552.log &
+nohup python mp_collect_server.py --gpu 1 --num_worker 10 --num_env 3 --max_steps 50 --port 29553 > ./logs/29553.log &
+nohup python mp_collect_server.py --gpu 1 --num_worker 10 --num_env 3 --max_steps 50 --port 29554 > ./logs/29554.log &
+nohup python mp_collect_server.py --gpu 1 --num_worker 10 --num_env 3 --max_steps 50 --port 29555 > ./logs/29555.log &
